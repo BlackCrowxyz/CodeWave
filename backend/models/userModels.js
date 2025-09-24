a5ca64e4e2879e24a8e87bb1e9806114ef8912d1
uniquecozmotec <unique@cozmotec.ie>
2025-09-24 14:35:03 +0100

    adding empty basic structure for user routes, user modesl and db migration

---
+++ backend/models/userModels.js
@@ -0,0 +1,19 @@
+import pool from '../config/db.js';
+
+export const getAllUsersService = async () => {
+}
+
+export const getUserByIdService = async (id) => {
+}
+
+export const createUserService = async (user) => {
+}
+
+export const updateUserByIdService = async (id, name, email) => {
+}
+
+export const deleteUserByIdService = async (id) => {
+}
+
+export const getUserByEmailService = async (email) => {
+}
